db01549b1e6ec5c7c831c65e225663dd24ed6b86
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-09-09 21:44:47 -0400

    added endpoints in index.js +  handlers folder

--- server/index.js
+++ server/index.js
@@ -1,30 +1,44 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const {
+  handleItemsByCategory,
+  handleItemsByCompany,
+  handleAllItems,
+} = require("./serverHandlers/handleItems");
+
+const { handlePurchase } = require("./serverHandlers/handlePurchase");
+const { handleSingleItem } = require("./serverHandlers/handleSingleItem");
 
 const PORT = 4000;
 
 express()
-  .use(function(req, res, next) {
+  .use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+  .use("/", express.static(__dirname + "/"))
 
   // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  .get("/items/filter/:category", handleItemsByCategory)
+  .get("/items/company/:companyId", handleItemsByCompany)
+  .get("/items/", handleAllItems)
+  .get("/items", handleSingleItem)
+
+  .patch("/buy", handlePurchase)
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
---
+++ server/serverHandlers/handleSingleItem.js
@@ -0,0 +1,5 @@
+const handleSingleItem = (req, res) => {};
+
+module.exports = {
+  handleSingleItem,
+};
