c3c7ad445768cf9d77b45fe1720f7fd8ee4957f4
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-09-12 17:00:06 -0400

    root folder fix

--- client/src/components/LandingPage/FilterSelector.js
+++ client/src/components/LandingPage/FilterSelector.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const FilterSelector = () => {
-  return <Wrapper>filters</Wrapper>;
+  return <Wrapper>Filters</Wrapper>;
 };
 
 const Wrapper = styled.div``;
