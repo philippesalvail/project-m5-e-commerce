a036231ec2eac5a6de48ad2509add86d661f2214
donnyesq <adonis@darmas.ca>
2020-09-13 12:15:33 -0400

    some more styling

--- client/src/components/Cart/CartItem.js
+++ client/src/components/Cart/CartItem.js
@@ -80,12 +80,13 @@ const StyledDiv = styled.div`
 
 const StyledH3 = styled.h3`
   color: #000;
-  margin: 0;
+  margin: 0 15px 0 0;
 `;
 
 const StyledH4 = styled.h4`
-  color: #fff;
-  margin: 0;
+  color: #000;
+  margin: 0 10px 10px 10px;
+  font-size: 18px;
 
   &:hover {
     cursor: pointer;
