14dea66c1fa816042b2537ed2506d1cee265aa30
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-09-14 19:46:46 -0400

    added react-icons, added cart icon and badge (not using redux yet)

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
+import CartButton from "./CartButton";
+
 const Header = () => {
   return (
     <Wrapper>
       <Title>620 SQUARE MILES</Title>
+      <CartButton />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.header`
   margin: 0;
+  margin-bottom: 20px;
   padding: 30px 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid black;
 `;
 
 const Title = styled.h1`
